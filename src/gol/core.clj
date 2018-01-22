20a102de791e00256940166b6c580fb89c7b4533
James Laverack <james@jameslaverack.com>
2018-01-23 00:02:58 +1100

    Run Game of Life

--- src/gol/core.clj
+++ src/gol/core.clj
@@ -6,9 +6,50 @@
   [width height]
   (matrix/fill (matrix/new-matrix height width) false))
 
+(defn rand-bool [] (= 0 (rand-int 2)))
 
+(defn randomise [world] (matrix/emap (fn [i] (rand-bool)) world))
+
+(def relative-coords (filter #(not (= [0 0] %)) (for [x [-1 0 1] y [-1 0 1]] (list x y))))
+
+(defn width [world] (matrix/column-count world))
+
+(defn height [world] (matrix/row-count world))
+
+(defn wrap-coord
+  [width height [x y]]
+  (list (mod x width) (mod y height)))
+
+(defn find-neighbors
+  [world loc]
+  (let [coords (map #(matrix/add loc %) relative-coords)
+        wrapped-coords (map (partial wrap-coord (width world) (height world)) coords)]
+    (map (fn [[x y]] (matrix/mget world x y)) wrapped-coords)))
+
+(def alive? true?)
+
+(defn step-cell
+  [[x y] cell neighbor-cells]
+  (let [alive-neighbors (count (filter alive? neighbor-cells))]
+    (if (alive? cell)
+      (cond
+        (< alive-neighbors 2) false
+        (> alive-neighbors 3) false
+        :else true)
+      (= alive-neighbors 3))))
+
+(defn step
+  [world]
+  (matrix/emap-indexed (fn [loc cell] (step-cell loc cell (find-neighbors world loc))) world))
+
+(defn display [world] (str world))
+
+(defn play-game
+  [world iterations]
+  (println (display world))
+  (if (> iterations 0) (recur (step world) (- iterations 1))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (play-game (randomise (seed 5 5)) 10))
